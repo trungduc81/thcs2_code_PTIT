a79c5c143ef1905a817a2648a7122d011c4532a5
Trần Đức Trung <ductrung081105@gmail.com>
2025-07-02 01:45:46 +0700

    Create C01004 - GIÁ TRỊ THẬP PHÂN.cpp

---
+++ C01004 - GIÁ TRỊ THẬP PHÂN.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#define ll long long
+
+int main(){
+	int t ; scanf("%d",&t) ; 
+	while(t--){
+		int n ; scanf("%d",&n) ; 
+	    double res = (double) 1 / n ; 
+	    printf("%.15f\n",res) ; 
+    }
+	return 0 ; 
+}
