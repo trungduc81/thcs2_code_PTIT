d2f583dcf1274fef77dd83b6accaf49a38dc4daf
Trần Đức Trung <ductrung081105@gmail.com>
2025-07-03 02:04:12 +0700

    Create C02001 - HÌNH VUÔNG DẤU *.cpp

---
+++ C02001 - HÌNH VUÔNG DẤU *.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#define ll long long 
+
+
+
+int main() {
+    int n ; scanf("%d",&n) ; 
+    for(int i = 1 ; i <= n ; i++){
+    	for(int j = 1 ; j <= n ; j++){
+    		printf("*") ; 
+		}
+		printf("\n") ; 
+	}
+}
+
