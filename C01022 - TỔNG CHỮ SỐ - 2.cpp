f7e55218d9a6534b32a0973c9518999e9381d0b2
Trần Đức Trung <ductrung081105@gmail.com>
2025-07-02 13:09:27 +0700

    Create C01022 - TỔNG CHỮ SỐ - 2.cpp

---
+++ C01022 - TỔNG CHỮ SỐ - 2.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+#define ll long long
+
+int main(){
+	int t ; scanf("%d",&t) ; 
+	while(t--){
+		int n ; scanf("%d",&n) ; 
+        int sum = 0 ; 
+		while(n>0){
+			sum += n%10 ; 
+			n /= 10 ; 
+		}
+		printf("%d\n",sum) ;  
+    }
+	return 0 ; 
+}
