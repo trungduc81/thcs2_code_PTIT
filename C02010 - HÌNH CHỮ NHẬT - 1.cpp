016eba7060daa3d0c13dfd24f87247caa91a62d3
Trần Đức Trung <ductrung081105@gmail.com>
2025-07-03 17:50:04 +0700

    Create C02010 - HÌNH CHỮ NHẬT - 1.cpp

---
+++ C02010 - HÌNH CHỮ NHẬT - 1.cpp
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+int main() {
+    int m , n ; scanf("%d %d",&m,&n) ; 
+    int k = 1 ; 
+    if(m<=n){
+    	for(int i = 1 ; i <= m ; i++){
+    		for(int j = k ; j <= n ; j++){
+    			printf("%d",j) ; 
+			}
+			for(int j = k-1 ; j >= 1 ;j--){
+				printf("%d",j) ; 
+			}
+			k++ ; 
+			printf("\n") ; 
+		}
+	}
+	else {
+		for(int i = 1 ; i <= n ; i++){
+    		for(int j = k ; j <= n ; j++){
+    			printf("%d",j) ; 
+			}
+			for(int j = k-1 ; j >= 1 ;j--){
+				printf("%d",j) ; 
+			}
+			k++ ; 
+			printf("\n") ; 
+		}
+		for(int i = n + 1 ; i <= m ; i++){
+			printf("%d",i) ; 
+			for(int j = n - 1 ; j >= 1 ; j--){
+				printf("%d",j) ;
+			} 
+			printf("\n") ; 
+		}
+		
+	}
+
+    return 0;
+}
+
