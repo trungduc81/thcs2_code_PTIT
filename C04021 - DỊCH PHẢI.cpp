0e7bb1a46a086a4ad3ddeb512c8236c059d1cf90
Trần Đức Trung <ductrung081105@gmail.com>
2025-07-07 16:56:54 +0700

    Create C04021 - DỊCH PHẢI.cpp

---
+++ C04021 - DỊCH PHẢI.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#define ll long long
+#define MAX 1000005
+
+void solve(){
+	int n ; scanf("%d",&n) ; 
+	int a[n] ; 
+	for(int i = 0 ; i < n ; i++){
+		scanf("%d",&a[i]) ; 
+	} 
+	int m ; scanf("%d",&m) ; 
+	for(int i =  n - m ; i < n ; i++) printf("%d ",a[i]) ; 
+	for(int i = 0 ; i < n - m ; i++) printf("%d ",a[i]) ; 
+} 
+
+int main(){ 
+    solve() ; 
+}
