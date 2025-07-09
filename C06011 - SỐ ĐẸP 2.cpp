c7b71d8fb1129f91c78060dc31557e7530c0dbd0
Trần Đức Trung <ductrung081105@gmail.com>
2025-07-09 13:59:16 +0700

    Create C06011 - SỐ ĐẸP 2.cpp

---
+++ C06011 - SỐ ĐẸP 2.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+
+int check(int a[] , int n){
+	if(a[0] != 8 && a[n-1] != 8) return 0 ; 
+	
+	int sum = 0 ; 
+	for(int i = 0 ; i < n ; i++) sum += a[i] ; 
+	if(sum%10 != 0) return 0 ; 
+	
+	for(int i = 0 ; i < (n+1)/2 ; i++){
+		if(a[i] != a[n-1-i]) return 0 ; 
+	}
+	return 1 ; 
+}
+
+void solve(){
+	char s[500] ; 
+	scanf("%s",s) ; 
+	int len = strlen(s) ; 
+	int a[500] ; 
+	for(int i = 0 ; i < len ; i++) a[i] = s[i] - '0' ; 
+	if(check(a,len)) printf("YES\n") ; 
+	else printf("NO\n") ; 
+}
+
+int main(){
+    int t ; scanf("%d",&t) ; 
+    while(t--){
+    	solve() ; 
+	}
+}
