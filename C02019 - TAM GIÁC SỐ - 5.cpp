10f61aed29ff21961f1a2c81eb04622fe38189bc
Trần Đức Trung <ductrung081105@gmail.com>
2025-07-11 03:03:15 +0700

    Create C02019 - TAM GIÁC SỐ - 5.cpp

---
+++ C02019 - TAM GIÁC SỐ - 5.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() {
+    int n ; scanf("%d",&n) ;  
+    for(int i = 1 ; i <= n ; i++){ 
+        int a = 2 ; 
+    	for(int j = 1 ; j <= 2*i - 1 ; j++){
+    		if(j<i) {
+    			printf("%d",a) ; 
+    			a += 2 ; 
+			}
+			else {
+				printf("%d",a) ; 
+				a -= 2 ; 
+			}
+		}
+		printf("\n") ; 
+	}
+}
+
+
