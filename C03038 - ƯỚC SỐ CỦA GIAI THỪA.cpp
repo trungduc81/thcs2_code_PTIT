e8de2a188267527f64033742d9bddaad46938e79
Trần Đức Trung <ductrung081105@gmail.com>
2025-07-15 16:36:02 +0700

    Create C03038 - ƯỚC SỐ CỦA GIAI THỪA.cpp

---
+++ C03038 - ƯỚC SỐ CỦA GIAI THỪA.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+#define ll long long
+
+
+void solve() {
+    int n , p ; scanf("%d %d",&n,&p) ; 
+    int cnt = 0 ; 
+    while(n>0){
+    	cnt += n/p  ; 
+    	n /= p ; 
+	}
+	printf("%d\n",cnt) ; 
+}
+
+int main() {
+    int t ; scanf("%d", &t) ;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
