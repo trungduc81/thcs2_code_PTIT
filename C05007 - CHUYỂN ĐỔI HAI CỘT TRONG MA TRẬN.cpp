521959627d21b4ec6ac00a64430e8c4cf32a01a4
Trần Đức Trung <ductrung081105@gmail.com>
2025-07-17 01:21:52 +0700

    Create C05007 - CHUYỂN ĐỔI HAI CỘT TRONG MA TRẬN.cpp

---
+++ C05007 - CHUYỂN ĐỔI HAI CỘT TRONG MA TRẬN.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+
+void swap(int *a , int *b){
+	int tmp = *a ; 
+	*a = *b ; 
+	*b = tmp ; 
+}
+
+int main(){
+	int m , n ; scanf("%d %d",&m,&n) ; 
+	int a[m][n] ; 
+	for(int i = 0 ; i < m ; i++){
+		for(int j = 0 ; j < n ; j++){
+			scanf("%d",&a[i][j]) ; 
+		}
+	}
+	int x , y ; scanf("%d %d",&x,&y) ; 
+	for(int i = 0 ; i < m ; i++){
+		swap(&a[i][x-1],&a[i][y-1]) ; 
+	}
+	for(int i = 0 ; i < m ; i++){
+		for(int j = 0 ; j < n ; j++){
+			printf("%d ",a[i][j]) ; 
+		}
+		printf("\n") ; 
+	}
+}
